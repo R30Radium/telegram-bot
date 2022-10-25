5e0c18e4c18bf5c746216c4154fbdbe4cf5211e7
R30Radium <R30Radium@gmail.ru>
2022-10-25 15:07:13 +0400

    course

---
+++ src/main/java/pro/sky/telegrambot/NotificationTask/NotificationTask.java
@@ -0,0 +1,78 @@
+package pro.sky.telegrambot.NotificationTask;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+public class NotificationTask {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    private long chatId;
+    private String notificationText;
+    private LocalDateTime dateTime;
+
+    public NotificationTask(long id, long chatId, String notificationText,
+                            LocalDateTime dateTime) {
+        this.id = id;
+        this.chatId = chatId;
+        this.notificationText = notificationText;
+        this.dateTime = dateTime;
+    }
+
+    public NotificationTask(){}
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(long chatId) {
+        this.chatId = chatId;
+    }
+
+    public String getNotificationText() {
+        return notificationText;
+    }
+
+    public void setNotificationText(String notificationText) {
+        this.notificationText = notificationText;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        NotificationTask that = (NotificationTask) o;
+        return chatId == that.chatId && Objects.equals(id, that.id) &&
+                Objects.equals(notificationText, that.notificationText) &&
+                Objects.equals(dateTime, that.dateTime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, chatId, notificationText, dateTime);
+    }
+
+    @Override
+    public String toString() {
+        return "NotificationTask{" +
+                "id=" + id +
+                ", chatId=" + chatId +
+                ", notificationText='" + notificationText + '\'' +
+                ", dateTime=" + dateTime +
+                '}';
+    }
+}
